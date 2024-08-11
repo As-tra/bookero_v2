c286094b4d9730065b1fe4d33db4fadfdeda5316
Assil <astradhdbjd@gmail.com>
2024-08-11 11:12:28 +0100

    create home view

---
+++ lib/utils/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for iconsMenu
+  /// assets/icons/menu.png
+  static const String iconsMenu = "assets/icons/menu.png";
+
+  /// Assets for iconsSearch
+  /// assets/icons/search.png
+  static const String iconsSearch = "assets/icons/search.png";
+}
+
